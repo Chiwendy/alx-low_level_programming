7a2fd200882c187261fd3a8f969c11d834741910
chiwendy <wenduogele1@gmail.com>
2022-09-15 18:28:04 -0700

    T 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Discription: print value of n; zero, positive ornegative
+ * Return: Accept 0 (success) 
+ *
+ */
+
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{ printf ("%d is zero\n", i);
+	}
+
+	else if (i > 0)
+	{ printf ("%d is positive\n, i);
+	}
+
+	else 
+	{ printf ("%d is negative\n, i)
+	}
+}
