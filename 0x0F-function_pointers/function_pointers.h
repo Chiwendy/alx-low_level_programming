c2e6d715baa47efa6b2d1878a8c9c2bf469f7e5a
Chiwendy <wenduogele1@gmail.com>
2023-10-13 15:03:50 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+void print_name(char *name, void (*f)(char *));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+int _putchar(char c);
+
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif 
